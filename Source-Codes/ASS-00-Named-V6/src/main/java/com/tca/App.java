1afead973c75c053390d3ede0897fe19e1172c8e
TCA <tca@example.com>
2025-10-28 13:10:43 +0530

    nmtest

---
+++ Source-Codes/ASS-00-Named-V6/src/main/java/com/tca/App.java
@@ -0,0 +1,129 @@
+package com.tca;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.MutationQuery;
+
+public class App 
+{
+    public static void main(String[] args) 
+    {
+        Configuration configuration=null;
+        SessionFactory sf=null;
+        Session session=null;
+        Transaction txn=null;  // DML Queries - insert , delete, Update
+        
+        try
+        {
+        		configuration = new Configuration();
+        		configuration.configure();             // hibernate.cfg.xml
+        		       		
+        		sf = configuration.buildSessionFactory();
+        	  	
+        		session = sf.openSession();	
+        		txn = session.beginTransaction();
+/*        
+ 			// Code  : @NamedQuery(name="GET_ALL_INFO",query="FROM Student")
+ 			
+        		Query<Student> query = session.createNamedQuery("GET_ALL_INFO", Student.class);
+    			List<Student> list = query.list();
+    			
+    			for(Student s : list)
+    			{
+    				System.out.println("Roll Number : " + s.getRno());
+    				System.out.println("Name        : " + s.getName());
+    				System.out.println("Percentage  : " + s.getPer());
+    				System.out.println("City        : " + s.getCity());
+    				System.out.println("---------------------------------------");
+    			}
+*/        		
+ 
+/*        		
+        		//@NamedQuery(name="GET_CITYWISE", query="FROM Student where city= :abc")
+    			
+        		Query<Student> query = session.createNamedQuery("GET_CITYWISE",Student.class);
+   			query.setParameter("abc","PUNE");
+   			List<Student> list = query.list(); 
+   			
+   			for(Student s : list)
+   			{
+   				System.out.println("Roll Number : " + s.getRno());
+   				System.out.println("Name        : " + s.getName());
+   				System.out.println("Percentage  : " + s.getPer());
+   				System.out.println("City        : " + s.getCity());
+   				System.out.println("---------------------------------------");
+   			}
+*/
+ 
+/*        		
+    			//@NamedQuery(name="MODIFY_STUDENT_CITY", query="UPDATE Student SET city=:newcity WHERE city=:oldcity")
+    			
+        		MutationQuery query = session.createNamedMutationQuery("MODIFY_STUDENT_CITY");
+    			query.setParameter("newcity", "PUNE");
+    			query.setParameter("oldcity", "POONA");
+    			int cnt = query.executeUpdate();
+    			System.out.println("No. of Records Updated :" +  cnt);
+*/
+
+/*	
+        		//@NamedQuery(name="REMOVE_STUDENT", query="DELETE FROM Student WHERE rno =:rollnumber")
+
+    			
+        		MutationQuery query = session.createNamedMutationQuery("REMOVE_STUDENT");
+    			query.setParameter("rollnumber", 102);
+    			int cnt = query.executeUpdate();
+    			System.out.println("No. of Records Deleted :" +  cnt);
+        		txn.commit();
+*/        		
+        		System.out.println("Work is done !!!");
+        		
+        		
+        }
+        catch(Exception e)
+        {	
+        		txn.rollback();
+        		e.printStackTrace();
+        }
+        finally
+        {
+        		session.close();
+        		sf.close();   // jdbc con.close()
+        }
+        
+        
+       
+        
+                   
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Source-Codes/ASS-00-Named-V6/src/main/java/com/tca/entities/Student.java
@@ -0,0 +1,66 @@
+package com.tca.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="student")
+@NamedQueries(
+		value = {
+				@NamedQuery(name="GET_ALL_INFO",query="FROM Student"),
+				@NamedQuery(name="GET_CITYWISE", query="FROM Student where city= :abc"),
+				@NamedQuery(name="MODIFY_STUDENT_CITY", query="UPDATE Student SET city=:newcity WHERE city=:oldcity"),
+				@NamedQuery(name="REMOVE_STUDENT", query="DELETE FROM Student WHERE rno =:rollnumber")
+			})
+public class Student 
+{
+	@Id
+	@Column(name="srno")
+	private Integer rno;
+	
+	@Column(name="sname")
+	private String name;
+
+	@Column(name="sper")
+	private Double per;
+	
+	@Column(name="scity")
+	private String city;
+	
+	public Student() {}
+	
+	public int getRno() {
+		return rno;
+	}
+	
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPer() {
+		return per;
+	}
+	public void setPer(double per) {
+		this.per = per;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	
+}
